3a29aad187db42b1246b7b076a378c8d1a99f04d
El Mahdi Farhat <efarhat@e3r9p1.1337.ma>
2024-08-13 18:45:04 +0100

    box + start in form component of signup page

--- frontend/src/pages/Auth/Signup.tsx
+++ frontend/src/pages/Auth/Signup.tsx
@@ -1,15 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import css from './Signup.module.css'
-import UserIcon from '../../../public/icons/login/bx-user.svg';
+import Welcome from './components/welcome/Welcome';
 import { FaRegUser } from "react-icons/fa";
-import { FaLock } from "react-icons/fa6";
-import { FaEnvelope } from "react-icons/fa";
+
+const Input = ({type, label}) => {
+  return (
+    <div className={css.inputArea}>
+      <input type={type} required placeholder={label}/>
+      {/* <div className={css.label}>{label}</div> */}
+    </div>
+  )
+}
+
 
 const Signup = () => {
   return (
-    <>
-      <div className={css.modal}>
+    <div className={css.boxContainer}>
+      <Welcome />
+      <div className={css.signupForm}>
+        <div className={css.entryArea}>
+          <FaRegUser className={css.userIcon}/>
+          <Input type="text" label="Username"/>
+          <Input type="email" label="Email"/>
+          <Input type="password" label="Password"/>
+        </div>
+        <button>Signup</button>
+        <p>Or Signup with</p>
+        <button>Login with google</button>
+        <button>Login with intra</button>
+        <div>
+          <p>Already have an account?</p>
+          <button>Signin</button>
+        </div>
+      </div>
+      {/* <div className={css.modal}>
         <div className={css.imageBox}></div>
 
         <div className={css.formBox}>
@@ -19,17 +44,17 @@ const Signup = () => {
               <p>create your account and enjoy the game</p>
             </div>
             <div className={css.inputs}>
-              <FaRegUser color='#f8f3e3' className={css.icons}/>
-              <input type="text" name="" id="" placeholder='username'/>
-              <FaEnvelope color='#f8f3e3' className={css.icons}/>
-              <input type="text" placeholder='email' />
-              <FaLock color='#f8f3e3' className='icons'/>
-              <input type="text" placeholder='password' />
+              <FaRegUser color='#f8f3e3' className={css.userIcon}/>
+              <input type="text" name="" id="username" placeholder='username'/>
+              <FaEnvelope color='#f8f3e3' className={css.emailIcon}/>
+              <input type="text" id='email' placeholder='email' />
+              <FaLock color='#f8f3e3' className={css.passIcon}/>
+              <input type="text" id='password' placeholder='password' />
             </div>
           </form>
         </div>
-      </div>
-    </>
+      </div> */}
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Auth/components/welcome/Welcome.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import css from './Welcome.module.css';
+
+const Welcome = () => {
+
+    return (
+        <div className={css.welcome}>
+          <h1>Welcome</h1>
+          <p>Create you account and enjot the game</p>
+        </div>
+      )
+}
+
+export default Welcome
