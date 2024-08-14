55eacf2833eb176b769fbcb8baa230362ad8628c
El Mahdi Farhat <efarhat@e3r9p1.1337.ma>
2024-08-14 19:18:39 +0100

    splitting the auth components

--- frontend/src/pages/Auth/Signup.tsx
+++ frontend/src/pages/Auth/Signup.tsx
@@ -1,61 +1,97 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client';
+// import React from 'react'
+// import ReactDOM from 'react-dom/client';
 import css from './Signup.module.css'
-import Welcome from './components/welcome/Welcome';
-import { FaRegUser } from "react-icons/fa";
+// import Welcome from './components/welcome/Welcome';
+// import { IconContext } from 'react-icons';
+// import { FaRegUser } from "react-icons/fa";
+// import { MdOutlineMail } from "react-icons/md";
+// import { MdOutlineLock } from "react-icons/md";
+// import { FcGoogle } from "react-icons/fc";
 
-const Input = ({type, label}) => {
-  return (
-    <div className={css.inputArea}>
-      <input type={type} required placeholder={label}/>
-      {/* <div className={css.label}>{label}</div> */}
-    </div>
-  )
-}
+// Components
+import AuthHeader from "./components/AuthHeader";
+import SignupForm from './components/SignupForm';
 
+
+// const Input = ({type, label, icon, cssIcon}) => {
+//   return (
+//     <>
+//       <IconContext.Provider value={{size: "2em"}}>
+//         <div className={cssIcon}>
+//           {icon}
+//         </div>
+//       </ IconContext.Provider>
+//       <div className={css.inputArea}>
+//         <input type={type} required placeholder={label}/>
+//         {/* <div className={css.label}>{label}</div> */}
+//       </div>
+//     </>
+//   )
+// }
 
 const Signup = () => {
   return (
     <div className={css.boxContainer}>
-      <Welcome />
-      <div className={css.signupForm}>
-        <div className={css.entryArea}>
-          <FaRegUser className={css.userIcon}/>
-          <Input type="text" label="Username"/>
-          <Input type="email" label="Email"/>
-          <Input type="password" label="Password"/>
-        </div>
-        <button>Signup</button>
-        <p>Or Signup with</p>
-        <button>Login with google</button>
-        <button>Login with intra</button>
-        <div>
-          <p>Already have an account?</p>
-          <button>Signin</button>
-        </div>
-      </div>
-      {/* <div className={css.modal}>
-        <div className={css.imageBox}></div>
-
-        <div className={css.formBox}>
-          <form action="">
-            <div className={css.welcome}>
-              <h1>Welcome</h1>
-              <p>create your account and enjoy the game</p>
-            </div>
-            <div className={css.inputs}>
-              <FaRegUser color='#f8f3e3' className={css.userIcon}/>
-              <input type="text" name="" id="username" placeholder='username'/>
-              <FaEnvelope color='#f8f3e3' className={css.emailIcon}/>
-              <input type="text" id='email' placeholder='email' />
-              <FaLock color='#f8f3e3' className={css.passIcon}/>
-              <input type="text" id='password' placeholder='password' />
-            </div>
-          </form>
-        </div>
-      </div> */}
+      <AuthHeader title='Welcome' description='Create you account and enjoy the game' />
+      <SignupForm />
     </div>
   )
 }
-
 export default Signup
+
+// const Signup = () => {
+//   return (
+//     <div className={css.boxContainer}>
+//       <Welcome />
+//       <div className={css.signupForm}>
+//         <div className={css.entryArea}>
+//           <Input type="text" label="Username" icon={<FaRegUser />} cssIcon={css.userIcon}/>
+//           <Input type="email" label="Email" icon={<MdOutlineMail />} cssIcon={css.emailIcon}/>
+//           <Input type="password" label="Password" icon={<MdOutlineLock />} cssIcon={css.passIcon}/>
+//         </div>
+//         <button className={css.signupBtn}>SignUp</button>
+//         <div className={css.orSignup}>
+//           <div className={css.line}></div><p>Or SignUp with</p><div className={css.line}></div>
+//         </div>
+//         <div className={css.signGoogle}>
+//           <button>
+//             <IconContext.Provider value={{size: "2em"}}>
+//               <FcGoogle className={css.google}/>
+//             </ IconContext.Provider>
+//             SignUp with google
+//           </button>
+//         </div>
+//         <button>
+
+//           SignUp with intra
+//         </button>
+//         <div>
+//           <p>Already have an account?</p>
+//           <button>Signin</button>
+//         </div>
+//       </div>
+//       {/* <div className={css.modal}>
+//         <div className={css.imageBox}></div>
+
+//         <div className={css.formBox}>
+//           <form action="">
+//             <div className={css.welcome}>
+//               <h1>Welcome</h1>
+//               <p>create your account and enjoy the game</p>
+//             </div>
+//             <div className={css.inputs}>
+//               <FaRegUser color='#f8f3e3' className={css.userIcon}/>
+//               <input type="text" name="" id="username" placeholder='username'/>
+//               <FaEnvelope color='#f8f3e3' className={css.emailIcon}/>
+//               <input type="text" id='email' placeholder='email' />
+//               <FaLock color='#f8f3e3' className={css.passIcon}/>
+//               <input type="text" id='password' placeholder='password' />
+//             </div>
+//           </form>
+//         </div>
+//       </div> */}
+//     </div>
+//   )
+// }
+
+
