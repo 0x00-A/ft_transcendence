bdde76de34cb78ed40f0426a00789feed074bff5
0x00-A <abdellatifigounad@gmail.com>
2024-08-16 23:26:25 +0100

    added a config file for constants and a file for helper function

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,4 +1,9 @@
+// loading bar
 export const LOADING_BAR_COLOR = '#f11946';
+
+// sidebar
 export const MENU_ICON_COLOR = '#F8F3E3';
 export const MENU_ACTIVE_ICON_COLOR = '#d6192e';
 export const MENU_ICON_SIZE = 32;
+export const DARK_MODE_ICON_SIZE = 26;
+export const SIDEBAR_RESIZE_WIDTH = 768;
