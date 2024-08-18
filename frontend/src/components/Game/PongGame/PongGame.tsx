f9125628886a4a54f4dcb51399ae7eacc01d42ee
0x00-A <abdellatifigounad@gmail.com>
2024-08-18 23:52:14 +0100

    update

--- frontend/src/components/Game/PongGame/PongGame.tsx
+++ frontend/src/components/Game/PongGame/PongGame.tsx
@@ -20,53 +20,13 @@ const Modes = [
 ];
 
 const PongGame = () => {
-  const [selectedMode, setSelectedMode] = useState<number | null>(null);
-
-  const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
-    const [ball, setBall] = useState({
-      radius: 20,
-      x: ctx.canvas.width / 2,
-      y: ctx.canvas.height / 2,
-    });
-
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    const drawBall = () => {
-      ctx.beginPath();
-      ctx.fillStyle = '#050505';
-      ctx.arc(
-        ctx.canvas.width - 20,
-        ctx.canvas.height - 20,
-        20,
-        0,
-        2 * Math.PI
-      );
-      ctx.fill();
-    };
-
-    drawBall();
-
-    function onmouseDown(e: MouseEvent) {
-      console.log(e);
-    }
-    function onmouseUp(e: MouseEvent) {
-      console.log(e);
-    }
-    function onmouseMove(e: MouseEvent) {
-      console.log(e);
-    }
-
-    ctx.canvas.addEventListener('mousedown', onmouseDown);
-    ctx.canvas.addEventListener('mouseup', onmouseUp);
-    ctx.canvas.addEventListener('mousemove', onmouseMove);
-
-    // ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  };
+  const [selectedMode, setSelectedMode] = useState<number | null>(0);
 
   if (selectedMode === 0)
     return (
       <>
         <p>Score</p>
-        <Canvas className={css.canvasContainer} draw={draw} />
+        <Canvas className={css.canvasContainer} />
       </>
     );
   return (
