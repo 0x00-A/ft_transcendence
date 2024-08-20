d0d55bb11dfab4fc66fd10009c955aff2c122833
Rachid El-ismaiyly <rel-isma@e1r1p4.1337.ma>
2024-08-20 18:54:43 +0100

    add button in header chat

--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,22 +1,29 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import { useState } from 'react';
 import css from './Chat.module.css';
 import ChatHeader from '../../components/chat/ChatHeader';
 
 const Chat = () => {
   const { isLoggedIn } = useAuth();
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsExpanded(!isExpanded);
+  };
 
   if (!isLoggedIn) {
     return <Navigate to="/signup" />;
   }
+
   return (
-    <main className={css.container}>
+    <main className={`${css.container} ${isExpanded ? css.expanded : ''}`}>
       <div className={css.sidebarLeft}></div>
       <div className={css.chatBody}>
-        <ChatHeader />
+        <ChatHeader toggleSidebar={toggleSidebar} isExpanded={isExpanded} />
         {/* Add more components for the chat messages here */}
       </div>
-      <div className={css.sidebarRight}></div>
+      {!isExpanded && <div className={css.sidebarRight}></div>}
     </main>
   );
 };
