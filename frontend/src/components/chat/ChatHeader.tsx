8b5261c6f42032cf321ff338aced7175ed0db17e
Rachid El-ismaiyly <rel-isma@e1r2p1.1337.ma>
2024-08-21 18:33:29 +0100

    link messages with header chat

--- frontend/src/components/chat/ChatHeader.tsx
+++ frontend/src/components/chat/ChatHeader.tsx
@@ -3,36 +3,43 @@ import css from './ChatHeader.module.css';
 import { FaCircleInfo } from 'react-icons/fa6';
 
 interface ChatHeaderProps {
-  isExpanded: boolean;
   toggleSidebar: () => void;
+  selectedMessage: {
+    avatar: string;
+    name: string;
+  } | null;
 }
 
 const ChatHeader: React.FC<ChatHeaderProps> = ({
   toggleSidebar,
-  isExpanded,
+  selectedMessage,
 }) => {
   return (
     <header className={css.chatHeader}>
-      <div className={css.chatHeaderContent}>
-        <div className={css.userInfo}>
-          <img
-            src="https://picsum.photos/200"
-            alt="User"
-            className={css.userAvatar}
-          />
-          <div className={css.userDetails}>
-            <h2 className={css.userName}>Rachid el ismaili</h2>
-            <p className={css.userStatus}>Typing...</p>
+      {selectedMessage ? (
+        <div className={css.chatHeaderContent}>
+          <div className={css.userInfo}>
+            <img
+              src={selectedMessage.avatar}
+              alt="User"
+              className={css.userAvatar}
+            />
+            <div className={css.userDetails}>
+              <h2 className={css.userName}>{selectedMessage.name}</h2>
+              <p className={css.userStatus}>Typing...</p>
+            </div>
+          </div>
+          <div className={css.chatActions}>
+            <button className={css.backButton} onClick={toggleSidebar}>
+              <i>
+                <FaCircleInfo color="#F8F3E3" />
+              </i>
+            </button>
           </div>
         </div>
-        <div className={css.chatActions}>
-          <button className={css.backButton} onClick={toggleSidebar}>
-            <i>
-              <FaCircleInfo color="#F8F3E3" />
-            </i>
-          </button>
-        </div>
-      </div>
+      ) : (
+        <div className={css.placeholderHeader}>Select a chat</div>
+      )}
     </header>
   );
 };
