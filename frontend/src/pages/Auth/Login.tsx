ff1f3c2b2d6c63e69ccb17345ea267082e295bd0
El Mahdi Farhat <efarhat@e1r1p3.1337.ma>
2024-08-25 21:03:02 +0100

    starting in backend side

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -1,25 +1,31 @@
-import css from './Login.module.css';
-import authCss from './Signup.module.css';
+// import css from './Login.module.css';
+// import authCss from './Signup.module.css';
 import AuthHeader from './components/AuthHeader';
-import AuthInput from './components/AuthInput';
-import AuthButton from './components/AuthButton';
+// import AuthInput from './components/AuthInput';
+// import AuthButton from './components/AuthButton';
 import UserIcon from "./assets/userIcon.svg";
 import PassIcon from "./assets/passIcon.svg";
 import ExternAuth from './components/ExternAuth';
 import AuthFooter from './components/AuthFooter';
+import { FormEvent } from 'react';
 
 const Login = () => {
+
+  // const handleSubmit = (event: FormEvent) => {
+  //   event.preventDefault();
+  // }
+
   return (
     <>
-        <AuthHeader
+        {/* <AuthHeader
           title="Welcome back"
           description=""
         />
-        <form className={authCss.entryArea}>
+        <form className={authCss.entryArea} onSubmit={handleSubmit}>
           <AuthInput type="text" pHolder="username" icon={UserIcon} />
           <AuthInput type="password" pHolder="password" icon={PassIcon} />
           <AuthButton className={authCss.authBtn}>Sign in</AuthButton>
-        </form>
+        </form> */}
         <ExternAuth />
     </>
   )
--- frontend/src/pages/Auth/Signup.tsx
+++ frontend/src/pages/Auth/Signup.tsx
@@ -1,17 +1,27 @@
-import css from './Signup.module.css';
-
+import css from './AuthForm.module.css';
 
 // Components
 import AuthHeader from "./components/AuthHeader";
-import AuthInput from './components/AuthInput';
-import AuthButton from './components/AuthButton';
 import ExternAuth from './components/ExternAuth';
 import UserIcon from "./assets/userIcon.svg";
 import EmailIcon from "./assets/emailIcon.svg";
 import PassIcon from "./assets/passIcon.svg";
-
+import { FieldValues, useForm } from "react-hook-form";
 
 const Signup = () => {
+
+  interface FormData {
+    username: string;
+    email: string;
+    password: string;
+  }
+
+  const { register, handleSubmit, formState: {errors, isValid} } = useForm<FormData>();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    console.log('submited');
+  }
 
   return (
     <>
@@ -19,14 +29,30 @@ const Signup = () => {
           title="Welcome"
           description="Create you account and enjoy the game"
         />
-        <form className={css.entryArea}>
-          <AuthInput type="text" pHolder="username" icon={UserIcon} />
-          <AuthInput type="email" pHolder="email" icon={EmailIcon} />
-          <AuthInput type="password" pHolder="password" icon={PassIcon} />
-          <AuthButton className={css.authBtn}>Signup</AuthButton>
+        <form className={css.entryArea} onSubmit={handleSubmit(onSubmit)}>
+          {/* <AuthInput type="text" pHolder="username" icon={UserIcon} /> */}
+          <div className={css.inputContainer}>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+            <img src={UserIcon} alt="X"/>
+            <input type="text" required placeholder="username" {...register('username', {required: true, minLength: 3})}/>
+          </div>
+          {/* <AuthInput type="email" pHolder="email" icon={EmailIcon} /> */}
+          <div className={css.inputContainer}>
+            <img src={EmailIcon} alt="X"/>
+            <input type="email" required placeholder="email" {...register('email')}/>
+            {/* {errors} */}
+          </div>
+          {/* <AuthInput type="password" pHolder="password" icon={PassIcon} /> */}
+          <div className={css.inputContainer}>
+            <img src={PassIcon} alt="X"/>
+            <input type="password" required placeholder="password" {...register('password')}/>
+          </div>
+          {/* <AuthButton className={css.authBtn}>Signup</AuthButton> */}
+          <button type="submit" className={css.authBtn}>
+            Signup
+          </button>
         </form>
         <ExternAuth />
       </>
-  ); 
+  );
 }
 export default Signup
