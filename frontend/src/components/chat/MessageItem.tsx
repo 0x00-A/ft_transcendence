34e015c13f57438fdb8c211b9e12a4a5ed4a9ec6
Rachid El-ismaiyly <rel-isma@e1r3p1.1337.ma>
2024-08-25 12:18:37 +0100

    fix button more

--- frontend/src/components/chat/MessageItem.tsx
+++ frontend/src/components/chat/MessageItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import css from './MessageItem.module.css';
 import { CgMoreO } from 'react-icons/cg';
 
@@ -10,50 +10,60 @@ interface MessageItemProps {
   unreadCount?: number;
   isSelected: boolean;
   onClick: () => void;
-  onMoreClick: (position: { top: number; left: number }) => void;
+  onMoreClick: (e: React.MouseEvent) => void;
+  showMoreIcon: boolean;
+  isActive: boolean;
 }
 
-const MessageItem: React.FC<MessageItemProps> = ({
-  avatar,
-  name,
-  lastMessage,
-  time,
-  unreadCount,
-  isSelected,
-  onClick,
-  onMoreClick,
-}) => {
-  const handleIconClick = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    const { top, left, height } = (
-      e.currentTarget as HTMLElement
-    ).getBoundingClientRect();
-    onMoreClick({ top: top + height, left });
-  };
-
-  return (
-    <div
-      className={`${css.messageItemWrapper} ${isSelected ? css.selected : ''}`}
-      onClick={onClick}
-    >
-      <div className={css.messageItem}>
-        <img src={avatar} alt={name} className={css.avatar} />
-        <div className={css.messageContent}>
-          <div className={css.messageHeader}>
-            <span className={css.name}>{name}</span>
-            <span className={css.time}>{time}</span>
+const MessageItem = forwardRef<HTMLDivElement, MessageItemProps>(
+  (
+    {
+      avatar,
+      name,
+      lastMessage,
+      time,
+      unreadCount,
+      isSelected,
+      onClick,
+      onMoreClick,
+      showMoreIcon,
+      isActive,
+    },
+    ref
+  ) => {
+    return (
+      <div
+        ref={ref}
+        className={`${css.messageItemWrapper} ${isSelected ? css.selected : ''}`}
+        onClick={onClick}
+      >
+        <div className={css.messageItem}>
+          <img src={avatar} alt={name} className={css.avatar} />
+          <div className={css.messageContent}>
+            <div className={css.messageHeader}>
+              <span className={css.name}>{name}</span>
+              <span className={css.time}>{time}</span>
+            </div>
+            <div className={css.messageBody}>
+              <span className={css.lastMessage}>{lastMessage}</span>
+              {unreadCount && unreadCount > 0 && (
+                <span className={css.unreadCount}>{unreadCount}</span>
+              )}
+            </div>
           </div>
-          <div className={css.messageBody}>
-            <span className={css.lastMessage}>{lastMessage}</span>
-            {unreadCount && unreadCount > 0 && (
-              <span className={css.unreadCount}>{unreadCount}</span>
-            )}
-          </div>
+          {showMoreIcon && (
+            <CgMoreO
+              className={`${css.icon} ${isActive ? css.active : ''}`}
+              onClick={(e) => {
+                e.stopPropagation();
+                onMoreClick(e);
+              }}
+            />
+          )}
         </div>
-        <CgMoreO className={css.icon} onClick={handleIconClick} />
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default MessageItem;
