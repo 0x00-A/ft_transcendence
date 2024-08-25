2442bda718a84cbd7b6acde601d45319ef4d8eb0
El Mahdi Farhat <efarhat@e1r1p3.1337.ma>
2024-08-25 21:03:02 +0100

    starting in backend side

--- frontend/src/pages/Auth/components/AuthButton.tsx
+++ frontend/src/pages/Auth/components/AuthButton.tsx
@@ -3,7 +3,7 @@ import css from "./AuthButton.module.css";
 
 const AuthButton = ({children, className=''}: PropsWithChildren<{className: string}>) => {
   return (
-    <button className={`${css.authBtn} ${className}`}>
+    <button type="submit" className={`${css.authBtn} ${className}`}>
         {children}
     </button>
   )
