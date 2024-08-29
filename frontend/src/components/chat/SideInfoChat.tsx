5cb5e5b32e314b5e54988b1418a7be3b12d22abe
Rachid El-ismaiyly <rel-isma@e3r4p4.1337.ma>
2024-08-29 13:05:32 +0100

    add sideinfo

---
+++ frontend/src/components/chat/SideInfoChat.tsx
@@ -0,0 +1,33 @@
+// import { useState, useRef, useEffect } from 'react';
+import React from 'react';
+import css from './SideInfoChat.module.css';
+import ProfileSection from './ProfileSection';
+import StatusSection from './StatusSection';
+import ButtonSection from './ButtonSection';
+
+interface Info {
+  avatar: string;
+  name: string;
+  lastMessage: string;
+  time: string;
+  unreadCount?: number;
+}
+
+interface SideInfoChatProps {
+  selectedMessage: Info;
+}
+
+const SideInfoChat: React.FC<SideInfoChatProps> = ({ selectedMessage }) => {
+  return (
+    <div className={css.sideInfoChat}>
+      <ProfileSection
+        avatarUrl={selectedMessage.avatar}
+        name={selectedMessage.name}
+      />
+      <StatusSection status="typing..." />
+      <ButtonSection />
+    </div>
+  );
+};
+
+export default SideInfoChat;
