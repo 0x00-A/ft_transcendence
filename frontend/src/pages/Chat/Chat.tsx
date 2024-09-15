7f27cc5fb0898a7cacc8735e7e7a2987d60969a1
rel-isma <relismaiyly@gmail.com>
2024-09-15 18:31:44 +0100

    add selected message in chat body

--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -11,6 +11,8 @@ import NoChatSelected from '../../components/chat/NoChatSelected';
 import SideInfoChat from '../../components/chat/SideInfoChat';
 import MessageInput from '../../components/chat/MessageInput';
 import MessageArea from '../../components/chat/MessageArea';
+import chatData from './chatdata';
+import messages from './messages';
 
 interface SelectedMessageProps {
   avatar: string;
@@ -19,91 +21,6 @@ interface SelectedMessageProps {
   time: string;
   unreadCount?: number;
 }
-
-const messages: SelectedMessageProps[] = [
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'oussama',
-    lastMessage: 'wfin akhouy l3ziz hani.....',
-    time: '21:15 PM',
-    unreadCount: 3,
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'abde latif',
-    lastMessage: 'wakhdmo adrari.....',
-    time: '21:15 PM',
-    unreadCount: 99,
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'mehadi f',
-    lastMessage: 'chni 9alih.....',
-    time: '21:15 PM',
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'yasmine',
-    lastMessage: 'nmchiw ntghdaw',
-    time: '21:15 PM',
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'hamza',
-    lastMessage: 'waaa3 lminisheeeel',
-    time: '21:15 PM',
-    unreadCount: 1,
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'hex01e',
-    lastMessage: 'appah',
-    time: '21:15 PM',
-    unreadCount: 99,
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'naima',
-    lastMessage: 'waa333',
-    time: '21:15 PM',
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'hex01e',
-    lastMessage: 'appah',
-    time: '21:15 PM',
-    unreadCount: 99,
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'hex01e',
-    lastMessage: 'appah',
-    time: '21:15 PM',
-    unreadCount: 99,
-  },
-  ,
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'naima',
-    lastMessage: 'waa333',
-    time: '21:15 PM',
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'hex01e',
-    lastMessage: 'appah',
-    time: '21:15 PM',
-    unreadCount: 99,
-  },
-  {
-    avatar: 'https://picsum.photos/200',
-    name: 'hex01e',
-    lastMessage: 'appah',
-    time: '21:15 PM',
-    unreadCount: 99,
-  },
-];
-
 interface ChatMessage {
   name: string;
   content: string;
@@ -112,57 +29,6 @@ interface ChatMessage {
   time: string;
 }
 
-const messageschats = {
-  oussama: [
-    {
-      name: 'oussama',
-      content: 'Hey, how are you?',
-      sender: false,
-      avatar: 'https://picsum.photos/200',
-      time: '21:15 PM',
-    },
-    {
-      name: 'rachid el ismaiyly',
-      content: 'I’m good, how about you?',
-      sender: true,
-      avatar: 'https://picsum.photos/200',
-      time: '21:16 PM',
-    },
-  ],
-  abdeLatif: [
-    {
-      name: 'abde latif',
-      content: 'Hey, how are you? cc',
-      sender: false,
-      avatar: 'https://picsum.photos/200',
-      time: '21:15 PM',
-    },
-    {
-      name: 'rachid el ismaiyly',
-      content: 'I’m good, how about you cc ?',
-      sender: true,
-      avatar: 'https://picsum.photos/200',
-      time: '21:16 PM',
-    },
-  ],
-};
-
-// const messageschats: ChatMessage[] = [
-//   {
-//     name: 'rachid el ismaiyly',
-//     content: 'Hey, how are you?',
-//     sender: false,
-//     avatar: 'https://picsum.photos/200',
-//     time: '21:15 PM',
-//   },
-//   {
-//     name: 'rachid el ismaiyly',
-//     content: 'I’m good, how about you?',
-//     sender: true,
-//     avatar: 'https://picsum.photos/200',
-//     time: '21:16 PM',
-//   },
-// ];
 const Chat = () => {
   const { isLoggedIn } = useAuth();
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
@@ -188,9 +54,12 @@ const Chat = () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
+
   useEffect(() => {
-    setChatMessages(messageschats);
-  }, []);
+    if (selectedMessage) {
+      setChatMessages(chatData[selectedMessage.name] || []);
+    }
+  }, [selectedMessage]);
 
   const toggleSidebar = () => {
     setIsExpanded(!isExpanded);
