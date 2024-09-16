4ad5d5d27ecfa9b925eb501bf55155ff779799a0
rel-isma <relismaiyly@gmail.com>
2024-09-16 18:05:33 +0100

    add send emoje

--- frontend/src/components/chat/MessageInput.tsx
+++ frontend/src/components/chat/MessageInput.tsx
@@ -15,6 +15,8 @@ const MessageInput = ({ onSendMessage }: MessageInputProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const emojiRef = useRef<HTMLDivElement>(null);
   const buttonEmojiRef = useRef<HTMLButtonElement>(null);
+  const [inputFocused, setInputFocused] = useState(false);
+  const [emojiSend, setEmojiSend] = useState('👍');
 
   const handleSend = () => {
     if (message.trim()) {
@@ -23,12 +25,15 @@ const MessageInput = ({ onSendMessage }: MessageInputProps) => {
       setMessage('');
 
       setIsFlying(true);
+      setInputFocused(false);
       setTimeout(() => {
         setIsFlying(false);
       }, 500);
       if (inputRef.current) {
         inputRef.current.focus();
       }
+    } else if (emojiSend) {
+      onSendMessage(emojiSend);
     }
   };
 
@@ -63,6 +68,22 @@ const MessageInput = ({ onSendMessage }: MessageInputProps) => {
     };
   }, []);
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value) {
+      setMessage(value);
+      setInputFocused(true);
+    }
+  };
+  // const handleInputFocus = () => {
+  // };
+
+  const handleInputBlur = () => {
+    if (!message.trim()) {
+      setInputFocused(false);
+    }
+  };
+
   return (
     <div className={css.messageInputWrapper}>
       {showEmojiPicker && (
@@ -76,8 +97,10 @@ const MessageInput = ({ onSendMessage }: MessageInputProps) => {
           type="text"
           placeholder="Write a message"
           value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          onChange={handleInputChange}
           onKeyDown={handleKeyDown}
+          // onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
           className={css.input}
         />
         <button
@@ -92,12 +115,18 @@ const MessageInput = ({ onSendMessage }: MessageInputProps) => {
         </button>
       </div>
 
-      <button
-        onClick={handleSend}
-        className={`${css.sendButton} ${isFlying ? css.animateIcon : ''}`}
-      >
-        <FaPaperPlane size={25} />
-      </button>
+      {inputFocused ? (
+        <button
+          onClick={handleSend}
+          className={`${css.sendButton} ${isFlying ? css.animateIcon : ''}`}
+        >
+          <FaPaperPlane size={25} />
+        </button>
+      ) : (
+        <button className={css.sendEmoji} onClick={handleSend}>
+          <span>👍</span>
+        </button>
+      )}
     </div>
   );
 };
