f5a65384b9df370cc09501c481fdd3ec97cb8599
rel-isma <relismaiyly@gmail.com>
2024-09-22 18:54:18 +0100

    finsh day 22

--- frontend/src/components/Friends/OnlineFriends.tsx
+++ frontend/src/components/Friends/OnlineFriends.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import css from './OnlineFriends.module.css';
 
+interface OnlineFriendsProps {
+  id: string;
+  username: string;
+  avatar: string;
+}
+
+const friendRequests: OnlineFriendsProps[] = [
+  { id: '1', username: 'abde latif', avatar: 'https://picsum.photos/202' },
+  {
+    id: '2',
+    username: 'imad king og 1337',
+    avatar: 'https://picsum.photos/206',
+  },
+  { id: '3', username: 'kasimo l9ra3', avatar: 'https://picsum.photos/207' },
+];
+
 const OnlineFriends: React.FC = () => {
   return (
     <div className={css.onlineFriends}>
-      <h1>Online Friends</h1>
-      {/* Add logic to list online friends */}
+      <h1 className={css.title}>Online Friends</h1>
+      <div className={css.list}>
+        {friendRequests.map((request) => (
+          <div key={request.id} className={css.requestCard}>
+            <img
+              src={request.avatar}
+              alt={request.username}
+              className={css.avatar}
+            />
+            <div className={css.userInfo}>
+              <span className={css.username}>{request.username}</span>
+              <span className={css.online}>Online</span>
+            </div>
+
+            <button
+              className={css.cancelButton}
+              // onClick={() => handleAccept(request.id)}
+            >
+              Message
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
