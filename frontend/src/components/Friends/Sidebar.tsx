0684dd4fce00d9f4e1467e0ad2fb12e59f3c6968
rel-isma <relismaiyly@gmail.com>
2024-09-23 15:28:15 +0100

    add animation in sidebar

--- frontend/src/components/Friends/Sidebar.tsx
+++ frontend/src/components/Friends/Sidebar.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './Sidebar.module.css';
+import { FaArrowCircleRight, FaArrowCircleLeft } from 'react-icons/fa';
 
 type ViewType = 'add' | 'all' | 'online' | 'requests' | 'sent' | 'blocked';
 
@@ -9,47 +10,64 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ setView, currentView }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <nav className={css.sidebar}>
+    <nav className={`${css.sidebar} ${isCollapsed ? css.collapsed : ''}`}>
       <div className={css.sidebarContent}>
         <button
           className={`${css.navButton} ${currentView === 'all' ? css.active : ''}`}
           onClick={() => setView('all')}
         >
-          <img src="/icons/friend/allFriends.svg" alt="All" /> All Friends
+          <img src="/icons/friend/allFriends.svg" alt="All" />
+          <span className={css.buttonText}>All Friends</span>
         </button>
         <button
           className={`${css.navButton} ${currentView === 'online' ? css.active : ''}`}
           onClick={() => setView('online')}
         >
           <img src="/icons/friend/onlineFriend.svg" alt="Online" />
-          Online Friends
+          <span className={css.buttonText}>Online Friends</span>
         </button>
         <button
           className={`${css.navButton} ${currentView === 'requests' ? css.active : ''}`}
           onClick={() => setView('requests')}
         >
-          <img src="/icons/friend/requestsFriend.svg" alt="Requests" /> Friend
-          Requests
+          <img src="/icons/friend/requestsFriend.svg" alt="Requests" />
+          <span className={css.buttonText}>Friend Requests</span>
         </button>
         <button
           className={`${css.navButton} ${currentView === 'sent' ? css.active : ''}`}
           onClick={() => setView('sent')}
         >
-          <img src="/icons/friend/sentRequests.svg" alt="Sent" /> Sent Requests
+          <img src="/icons/friend/sentRequests.svg" alt="Sent" />
+          <span className={css.buttonText}>Sent Requests</span>
         </button>
         <button
           className={`${css.navButton} ${currentView === 'blocked' ? css.active : ''}`}
           onClick={() => setView('blocked')}
         >
-          <img src="/icons/friend/blockList.svg" alt="Blocked" /> Blocked List
+          <img src="/icons/friend/blockList.svg" alt="Blocked" />
+          <span className={css.buttonText}>Blocked List</span>
         </button>
       </div>
       <button
         className={`${css.addFriendButton} ${currentView === 'add' ? css.active : ''}`}
         onClick={() => setView('add')}
       >
-        <img src="/icons/friend/addFriend.svg" alt="Add" /> Add Friend
+        <img src="/icons/friend/addFriend.svg" alt="Add" />
+        <span className={css.buttonText}>Add Friend</span>
+      </button>
+      <button className={css.collapseButton} onClick={toggleCollapse}>
+        {isCollapsed ? (
+          <FaArrowCircleRight size={24} />
+        ) : (
+          <FaArrowCircleLeft size={24} />
+        )}
       </button>
     </nav>
   );
