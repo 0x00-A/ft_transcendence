7ffcf4513d8f308ab586f04ce4a3c53e1a336d56
rel-isma <relismaiyly@gmail.com>
2024-09-23 13:31:12 +0100

    add compont all friends

--- frontend/src/components/Friends/AllFriends.tsx
+++ frontend/src/components/Friends/AllFriends.tsx
@@ -1,11 +1,67 @@
 import React from 'react';
 import css from './AllFriends.module.css';
 
+interface Friend {
+  id: string;
+  username: string;
+  avatar: string;
+  isOnline: boolean;
+}
+
+const allFriends: Friend[] = [
+  {
+    id: '1',
+    username: 'abde latif',
+    avatar: 'https://picsum.photos/215',
+    isOnline: true,
+  },
+  {
+    id: '2',
+    username: 'abde latif',
+    avatar: 'https://picsum.photos/214',
+    isOnline: false,
+  },
+  {
+    id: '3',
+    username: 'abde latif',
+    avatar: 'https://picsum.photos/212',
+    isOnline: true,
+  },
+  {
+    id: '4',
+    username: 'abde latif',
+    avatar: 'https://picsum.photos/213',
+    isOnline: false,
+  },
+];
+
 const AllFriends: React.FC = () => {
   return (
     <div className={css.allFriends}>
-      <h1>All Friends</h1>
-      {}
+      <h1 className={css.title}>All Friends</h1>
+      <div className={css.friendList}>
+        {allFriends.map((friend) => (
+          <div key={friend.id} className={css.friendCard}>
+            <img
+              src={friend.avatar}
+              alt={friend.username}
+              className={css.avatar}
+            />
+            <div className={css.userInfo}>
+              <span className={css.username}>{friend.username}</span>
+              <span className={css.status}>
+                {friend.isOnline ? 'Online' : 'Offline'}
+              </span>
+            </div>
+            <div className={css.actions}>
+              <button className={css.actionButton}>Block</button>
+              <button className={css.actionButton}>Message</button>
+              <button className={css.actionButton}>Invite</button>
+              <button className={css.actionButton}>View Profile</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
