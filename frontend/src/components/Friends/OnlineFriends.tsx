4f7af3c9a5f6135d7e53aec97be97bbb19a67f43
rel-isma <relismaiyly@gmail.com>
2024-09-25 18:17:39 +0100

    active button message in online friends

--- frontend/src/components/Friends/OnlineFriends.tsx
+++ frontend/src/components/Friends/OnlineFriends.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import css from './OnlineFriends.module.css';
+import { useNavigate } from 'react-router-dom';
 
 interface Friend {
   id: string;
@@ -8,14 +9,24 @@ interface Friend {
 }
 
 const onlineFriends: Friend[] = [
-  { id: '1', username: 'abde latif', avatar: 'https://picsum.photos/209' },
-  { id: '2', username: 'abde latif', avatar: 'https://picsum.photos/209' },
-  { id: '3', username: 'abde latif', avatar: 'https://picsum.photos/209' },
-  { id: '4', username: 'abde latif', avatar: 'https://picsum.photos/209' },
-  { id: '5', username: 'abde latif', avatar: 'https://picsum.photos/209' },
+  { id: '1', username: 'yasmine', avatar: 'https://picsum.photos/209' },
+  { id: '2', username: 'oussama', avatar: 'https://picsum.photos/209' },
+  { id: '3', username: 'mehadi f', avatar: 'https://picsum.photos/209' },
+  {
+    id: '4',
+    username: 'imad king of 1337',
+    avatar: 'https://picsum.photos/209',
+  },
+  { id: '5', username: 'kasimo l9re3', avatar: 'https://picsum.photos/209' },
 ];
 
 const OnlineFriends: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleMessageClick = (friend: Friend) => {
+    navigate('/chat', { state: { selectedFriend: friend } });
+  };
+
   return (
     <div className={css.onlineFriends}>
       <h1 className={css.title}>Online Friends</h1>
@@ -32,7 +43,12 @@ const OnlineFriends: React.FC = () => {
               <span className={css.online}>Online</span>
             </div>
             <div className={css.actions}>
-              <button className={css.actionButton}>Message</button>
+              <button
+                className={css.actionButton}
+                onClick={() => handleMessageClick(friend)}
+              >
+                Message
+              </button>
               <button className={css.actionButton}>Invite</button>
               <button className={css.actionButton}>View Profile</button>
             </div>
