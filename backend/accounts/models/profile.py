5cf53d076c0834b6033ca300d40a2b6d0e4038bd
0x00-A <abdellatifigounad@gmail.com>
2024-10-27 19:34:01 +0100

    models update

--- backend/accounts/models/profile.py
+++ backend/accounts/models/profile.py
@@ -1,12 +1,16 @@
 from django.db import models
 from .user import User
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='media/avatars/', default='defaultAvatar.svg')
+    avatar = models.ImageField(
+        upload_to='media/avatars/', default='defaultAvatar.svg')
     age = models.PositiveSmallIntegerField(null=True, blank=True)
     level = models.PositiveSmallIntegerField(null=True, blank=True)
     friends = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+    stats = models.JSONField(default=dict, blank=True)
 
     def __str__(self) -> str:
         return self.user.username
