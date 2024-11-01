d56db988cc4923b72894940c40e6c1689633091e
rel-isma <relismaiyly@gmail.com>
2024-11-01 11:39:54 +0100

    add get_friend_status

--- backend/accounts/views/profile.py
+++ backend/accounts/views/profile.py
@@ -9,6 +9,21 @@ User = get_user_model()
 from ..models import Profile, FriendRequest
 from ..serializers import ProfileSerializer
 
+def get_friend_status(current_user, other_user):
+    """Helper function to determine the friend request status between two users."""
+    friend_request = FriendRequest.objects.filter(
+        Q(sender=current_user, receiver=other_user) | Q(sender=other_user, receiver=current_user)
+    ).first()
+    
+    if friend_request:
+        if friend_request.status == 'accepted':
+            return 'accepted'
+        elif friend_request.receiver == current_user:
+            return friend_request.status
+        else:
+            return 'cancel'
+    return 'Add Friend'
+
 class AllUsersView(APIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
@@ -17,39 +32,21 @@ class AllUsersView(APIView):
         try:
             users = User.objects.exclude(username=request.user.username)
             user_data_with_status = []
+            
             for user in users:
                 user_data = UserSerializer(user).data
-                try:
-                    friend_request = FriendRequest.objects.filter(
-                        Q(sender=request.user, receiver=user) | Q(sender=user, receiver=request.user)
-                    ).first()
-                    if friend_request:
-                        if friend_request.status == 'accepted':
-                            user_data['friend_request_status'] = 'accepted'
-                        elif friend_request.receiver == request.user:
-                            user_data['friend_request_status'] = friend_request.status
-                        else:
-                            user_data['friend_request_status'] = 'cancel'
-                    else:
-                        user_data['friend_request_status'] = 'Add Friend'
-                    # if friend_request:
-                    #     if friend_request.receiver == request.user:
-                    #         user_data['friend_request_status'] = friend_request.status
-                    #     else:
-                    #         user_data['friend_request_status'] = 'cancel'
-                    # else:
-                    #     user_data['friend_request_status'] = 'Add Friend'
-                except FriendRequest.DoesNotExist:
-                    user_data['friend_request_status'] = 'Add Friend'
+                user_data['friend_request_status'] = get_friend_status(request.user, user)
                 user_data_with_status.append(user_data)
+            
             return Response(user_data_with_status, status=status.HTTP_200_OK)
+
         except User.DoesNotExist:
             return Response(
                 {'error': 'User profile not found'},
                 status=status.HTTP_404_NOT_FOUND
             )
-        except Exception:
+        except Exception as e:
             return Response(
-                {'error': 'Internal server error'},
+                {'error': f'Internal server error: {str(e)}'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
