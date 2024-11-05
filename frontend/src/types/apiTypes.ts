9a94f973fa5eb428d83632b3ca3e1d13462b9a62
0x00-A <abdellatifigounad@gmail.com>
2024-11-05 02:38:45 +0100

    update

--- frontend/src/types/apiTypes.ts
+++ frontend/src/types/apiTypes.ts
@@ -10,3 +10,19 @@ export interface NewPostData {
   body: string;
   userId: number;
 }
+
+export interface User {
+  id: number;
+  username: string;
+}
+
+export interface Tournament {
+  id: number;
+  name: string;
+  creator: User;
+  created_at: string; // Use `Date` if you plan to parse it into a Date object
+  participants_count: number;
+  number_of_players: number;
+  user_id: number;
+  players: number[];
+}
