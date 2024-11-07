01d91b94b6052840de275cd22fdb3593bed98451
rel-isma <relismaiyly@gmail.com>
2024-11-07 19:50:24 +0100

    work dashboard v3

--- frontend/src/components/Welcome.tsx
+++ frontend/src/components/Welcome.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
 import styles from './Welcome.module.css';
 
 const Welcome = () => {
   return (
     <div className={styles.welcome}>
+      <div className={styles.content}>
+        <div>
+          <h2 className={styles.title}>FT-PONG</h2>
+          <p className={styles.description}>
+            <span>Ready to start   </span>   your next match and climb  <span>the leaderboard</span> 
+          </p>
+        </div>
+        <button className={styles.playButton}>Play now</button>
+      </div>
       <div className={styles.playerImage}>
         <img src="/public/rr.png" alt="Player playing ping-pong" />
-      </div>
-      <div className={styles.content}>
-        <h2 className={styles.title}>FT-PONG</h2>
-        <p className={styles.description}>
-          Ready to start your next match and climb the leaderboard?
-        </p>
-        <button className={styles.playButton}>Play now</button>
       </div>
     </div>
   );
