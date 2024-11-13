494a7124fb33f2d662ccb3005ea33458b9733694
0x00-A <abdellatifigounad@gmail.com>
2024-11-13 18:23:13 +0100

    userContext provider

--- frontend/src/types/apiTypes.ts
+++ frontend/src/types/apiTypes.ts
@@ -11,9 +11,24 @@ export interface NewPostData {
   userId: number;
 }
 
+export interface Profile {
+  user: number;
+  avatar: string;
+  age: number | null;
+  level: number | null;
+  stats: Record<string, unknown>;
+  is_online: boolean;
+}
+
 export interface User {
   id: number;
   username: string;
+  email: string;
+  is_oauth_user: boolean;
+  first_name: string;
+  last_name: string;
+  profile: Profile;
+  friend_request_status?: "accepted" | "pending" | "Add Friend" | "cancel";
 }
 
 export interface Tournament {
