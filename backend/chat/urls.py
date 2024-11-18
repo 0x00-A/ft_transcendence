eeb59309da217a301d06dc2d5df547d291aad4c3
rel-isma <relismaiyly@gmail.com>
2024-11-18 22:27:31 +0100

    add now get messges

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -7,8 +7,8 @@ from .views import (
 urlpatterns = [
     # Conversation
     path('conversations/', GetConversationsView.as_view(), name='get-conversations'),
+    path('conversations/create/', CreateConversationView.as_view(), name='create-conversation'),
     # Message
     path('conversations/<int:conversation_id>/messages/', GetMessagesView.as_view(), name='get-messages'),
-    path('conversations/create/', CreateConversationView.as_view(), name='create-conversation'),
     path('messages/create/', CreateMessageView.as_view(), name='create-message'),
 ]
