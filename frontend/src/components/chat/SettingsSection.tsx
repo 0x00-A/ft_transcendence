d43ca69933d94cebc61dafd4abbdf82dd0fb0e87
rel-isma <relismaiyly@gmail.com>
2024-12-09 15:00:27 +0100

    add mutual Friends

--- frontend/src/components/chat/SettingsSection.tsx
+++ frontend/src/components/chat/SettingsSection.tsx
@@ -1,33 +1,36 @@
 import { useState } from 'react';
 import css from './SettingsSection.module.css';
-import { FaBell, FaBan } from 'react-icons/fa';
-import { FaAngleDown, FaAngleUp } from 'react-icons/fa6';
+import { FaBan } from 'react-icons/fa';
 import { useSelectedConversation } from '@/contexts/SelectedConversationContext';
 import { useUser } from '@/contexts/UserContext';
 import { useWebSocketChat } from '@/contexts/WebSocketChatProvider';
-
-
+import { ChevronRight, ChevronDown } from 'lucide-react';
+import { useGetData } from '@/api/apiHooks';
+import { MutualFriend, Friends } from '@/types/apiTypes';
+import { useNavigate } from 'react-router-dom';
+import MutualFriendSkeleton from './MutualFriendSkeleton';
 
 const SettingsSection = () => {
   const [isPrivacyOpen, setIsPrivacyOpen] = useState(false);
   const { selectedConversation } = useSelectedConversation();
   const { user } = useUser();
   const { toggleBlockStatus } = useWebSocketChat();
-
-
-
+  const [isMutualFriendsOpen, setIsMutualFriendsOpen] = useState(false);
+  const { data: mutualFriends, isLoading: mutualIsLoading } = useGetData<MutualFriend>(
+    `/friends/mutual/${selectedConversation?.name}`
+  );
+  const navigate = useNavigate();
 
   const togglePrivacy = () => setIsPrivacyOpen(!isPrivacyOpen);
-
+  const toggleMutualFriends = () => setIsMutualFriendsOpen(!isMutualFriendsOpen);
 
   const handleBlock = async () => {
     if (user?.id !== undefined) {
-      if (selectedConversation?.block_status == "blocker")
+      if (selectedConversation?.block_status == 'blocker')
         toggleBlockStatus(selectedConversation.id, user.id, selectedConversation.user_id, false);
-      else if (selectedConversation?.block_status == "blocked")
+      else if (selectedConversation?.block_status == 'blocked')
         toggleBlockStatus(selectedConversation.id, user.id, selectedConversation.user_id, true);
-      else
-        toggleBlockStatus(selectedConversation!.id, user.id, selectedConversation!.user_id, true);
+      else toggleBlockStatus(selectedConversation!.id, user.id, selectedConversation!.user_id, true);
     }
   };
 
@@ -36,17 +39,40 @@ const SettingsSection = () => {
       <div className={css.section}>
         <div className={css.sectionHeader} onClick={togglePrivacy}>
           Privacy
-          <span className={isPrivacyOpen ? css.iconRotated : ''}>
-            {isPrivacyOpen ? <FaAngleDown /> : <FaAngleUp />}
-          </span>
+          {isPrivacyOpen ? <ChevronDown /> : <ChevronRight />}
         </div>
         {isPrivacyOpen && (
           <div className={css.sectionContent}>
-            <div
-              className={css.Item}
-              onClick={() => handleBlock()}
-              >
-              <FaBan /><span> {selectedConversation?.block_status === "blocker" ? "Unblock" : "Block"} </span>
+            <div className={css.Item} onClick={() => handleBlock()}>
+              <FaBan />
+              <span> {selectedConversation?.block_status === 'blocker' ? 'Unblock' : 'Block'} </span>
+            </div>
+          </div>
+        )}
+      </div>
+        <hr />
+      <div className={css.section}>
+        <div className={css.sectionHeader} onClick={toggleMutualFriends}>
+          Mutual Friends
+          {isMutualFriendsOpen ? <ChevronDown /> : <ChevronRight />}
+        </div>
+        {isMutualFriendsOpen && (
+          <div className={css.sectionContent}>
+            <div className={css.mutualFriendsContainer}>
+              {mutualIsLoading
+                  ?  <MutualFriendSkeleton  />
+                  : mutualFriends?.mutual_friends.map((friend: Friends) => (
+                      <div
+                        key={friend.id}
+                        className={css.mutualFriendItem}
+                        onClick={() => navigate(`/profile/${friend.username}`)}
+                      >
+                        <div className={`${css.avatar} ${friend.profile.is_online ? css.online : css.offline}`}>
+                          <img src={friend.profile.avatar} alt={friend.username} />
+                        </div>
+                        <span>{friend.username}</span>
+                      </div>
+                    ))}
             </div>
           </div>
         )}
