68aa6e562e08ed39f199b45c2920a0fab06c4615
0x00-A <abdellatifigounad@gmail.com>
2024-12-11 13:35:16 +0100

    You know, for research

--- elk/setup/entrypoint.sh
+++ elk/setup/entrypoint.sh
@@ -27,7 +27,7 @@ until curl -s -X POST --cacert config/certs/ca/ca.crt \
     -u "elastic:${ELASTIC_PASSWORD}" \
     -H "Content-Type: application/json" \
     https://es:9200/_security/user/kibana_system/_password \
-    -d '{"password":"${KIBANA_PASSWORD}"}' | grep -q "^{}"; do sleep 10; done;
+    -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
 
 
 # echo "Setting logs template";
