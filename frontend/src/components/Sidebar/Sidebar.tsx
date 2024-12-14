c706ed5352a797c9a0656b42b29ebe753f46eca3
rel-isma <relismaiyly@gmail.com>
2024-12-14 02:21:33 +0100

    work in Multiple language supports.

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import css from './Sidebar.module.css';
 import { SidebarMenu } from './components/SidebarMenu/SidebarMenu';
 import { useEffect, useState } from 'react';
 import { IoLogOut } from 'react-icons/io5';
+import Flag from 'react-world-flags';
 import { useAuth } from '../../contexts/AuthContext';
 import ConfirmationModal from '../ConfirmationModal/ConfirmationModal';
 import { useLoadingBar } from '../../contexts/LoadingBarContext';
@@ -15,6 +16,7 @@ import {
 import apiClient from '../../api/apiClient';
 import { API_LOGOUT_URL } from '@/api/apiConfig';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 export default function Sidebar() {
   // const [open, setOpen] = useState(true);
@@ -28,7 +30,10 @@ export default function Sidebar() {
     };
   }, []);
 
+  const { i18n } = useTranslation();
   const [showConfirm, setShowConfirm] = useState(false);
+  const [showLangPopup, setShowLangPopup] = useState(false);
+  const [selectedLang, setSelectedLang] = useState('English');
   // const [isLoggingOut, setIsLoggingOut] = useState(false);
 
   const handleLogoutClick = () => {
@@ -70,6 +75,14 @@ export default function Sidebar() {
   //   };
   // }, []);
 
+
+  const changeLanguage = (lang: string) => {
+    setSelectedLang(lang); // Set selected language
+    i18n.changeLanguage(lang); // Change the language using i18n
+    localStorage.setItem('lang', lang); // Store selected language in localStorage
+    setShowLangPopup(false); // Close popup after language selection
+  };
+
   return (
     <aside className={`${css.sidebar}`}>
       {/* <img
@@ -84,6 +97,27 @@ export default function Sidebar() {
       <div className={css.menuBox}>
         <SidebarMenu />
         <div className={`${css.bottom}`}>
+        <div
+          className={css.languageSwitcher}
+          onClick={() => setShowLangPopup((prevState) => !prevState)}
+        >
+          <Flag
+            code={selectedLang === 'en' ? 'US' : selectedLang === 'es' ? 'ES' : 'MA'}
+            width={32}
+            height={32}
+          />
+          {showLangPopup && (
+            <div className={css.languagePopup}>
+              <ul>
+                <li onClick={() => changeLanguage('en')}> <Flag className={css.flags} code='US'/> English </li>
+                <li onClick={() => changeLanguage('es')}> <Flag className={css.flags} code='ES'/> Español </li>
+                <li onClick={() => changeLanguage('tamazight')}> <Flag className={css.flags} code='MA'/> Tamazight </li>
+              </ul>
+            </div>
+          )}
+          <p>{selectedLang}</p>
+        </div>
+
           <ThemeToggle className={css.darkMode}></ThemeToggle>
           <div className={css.logout} onClick={handleLogoutClick}>
             <IoLogOut size={MENU_ICON_SIZE} color={MENU_ICON_COLOR} />
