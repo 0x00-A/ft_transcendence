3dc79e823a6001ab32c4645731dd9ac1643ad193
Abdelatif Igounad <aigounad@e1r6p13.1337.ma>
2024-12-15 12:05:49 +0100

    update

--- frontend/src/utils/getApiUrl.ts
+++ frontend/src/utils/getApiUrl.ts
@@ -1,8 +1,8 @@
-import { SERVER_PORT } from "@/config/constants";
+import { SERVER_PORT } from '@/config/constants';
 
 export const getApiUrl = (endpoint: string) => {
-    const protocol = window.location.protocol;
-    const host = window.location.hostname;
-    const port = window.location.port ? `:${SERVER_PORT}` : '';
-    return `${protocol}//${host}${port}/api${endpoint}`;
+  const protocol = window.location.protocol;
+  const host = window.location.hostname;
+  const port = window.location.port ? `:${SERVER_PORT}` : '';
+  return `${protocol}//${host}${port}/api${endpoint}`;
 };
