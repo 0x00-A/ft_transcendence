43034e348711ad234b89b7eb09cd4c4703ffbb86
rel-isma <relismaiyly@gmail.com>
2024-12-19 21:50:41 +0100

    add min heghit to page chat and friends

--- frontend/src/api/chatApi.ts
+++ frontend/src/api/chatApi.ts
@@ -12,16 +12,3 @@ export const apiCreateConversation = async (user2Id: string) => {
         }
     }
 };
-
-export const apiDeleteConversation = async (conversationId: Number) => {
-    try {
-        const response = await apiClient.delete(`/chat/conversations/${conversationId}/delete/`);
-        return response.data || 'Conversation deleted successfully';
-    } catch (error: any) {
-        if (error.response && error.response.data && error.response.data.error) {
-            throw new Error(error.response.data.error);
-        } else {
-            throw new Error('Failed to delete conversation');
-        }
-    }
-};
