9732110a0c25d63df85d851fa207475d4878ece3
rel-isma <relismaiyly@gmail.com>
2024-12-27 16:07:40 +0100

    merge with main

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -1,24 +1,8 @@
 export const sleep = (delay: number) =>
   new Promise((resolve) => setTimeout(resolve, delay));
 
-const tamazightMonths = [
-  "ⵉⵏⵏⴰⵢⵔ", "ⴱⵕⴰⵢⵕ", "ⵎⴰⵕⵚ", "ⵉⴱⵔⵉⵔ", "ⵎⴰⵢⵢⵓ", "ⵢⵓⵏⵢⵓ", "ⵢⵓⵍⵢⵓⵣ", "ⵖⵓⵛⵜ", "ⵛⵓⵜⴰⵏⴱⵉⵔ", "ⴽⵟⵓⴱⵕ", "ⵏⵓⵡⴰⵏⴱⵉⵔ", "ⴷⵓⵊⴰⵏⴱⵉⵔ"
-];
-
-export const formatDateTamazight = (dateString: Date) => {
-  const date = new Date(dateString);
-  const month = tamazightMonths[date.getMonth()];
-  const day = date.getDate();
-  const hour = date.getHours() % 12 || 12;
-  const minute = date.getMinutes();
-  const period = date.getHours() >= 12 ? "ⴰⵜⵉⴼⵓⵙ" : "ⵜⵉⴽⵔⵉ";
-
-  return `${month} ${day}, ${hour}:${minute} ${period}`;
-};
-
 export const formatDate = (dateString: Date, locale: string = 'en') => {
   const date = new Date(dateString);
-  if (locale === "tmazight") return formatDateTamazight(dateString);
   return date.toLocaleString(locale, {
     month: 'short',
     day: 'numeric',
