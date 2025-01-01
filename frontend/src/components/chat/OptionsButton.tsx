250e6860555fbaae1ee22e3710abf8407dbb74aa
rel-isma <relismaiyly@gmail.com>
2025-01-01 19:32:31 +0100

    add profile view

--- frontend/src/components/chat/OptionsButton.tsx
+++ frontend/src/components/chat/OptionsButton.tsx
@@ -1,18 +1,23 @@
 import css from './OptionsButton.module.css';
-import NewMessage from './NewMessage';
 import { useTranslation } from 'react-i18next';
+import { User } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 
 const OptionsButton = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
 
 
   return (
     <div className={css.optionsButtonContainer}>
       <h2 className={css.title}>{t('optionsButton')}</h2>
-
       <div className={css.sideButtons}>
-        <NewMessage  />
+        <div className={css.newMessage} onClick={() => navigate(`/friends?view=add`)}>
+          <div className={css.btnLabel}>{t('addConversation')}</div>
+          <User size={30}/>
+        </div>
       </div>
     </div>
   );
