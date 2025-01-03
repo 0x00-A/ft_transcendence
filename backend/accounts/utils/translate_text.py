ee0460e1c5b587a1334038950e88b10a8ff31ab0
rel-isma <relismaiyly@gmail.com>
2025-01-03 17:30:58 +0100

    translator not work good in backend

--- backend/accounts/utils/translate_text.py
+++ backend/accounts/utils/translate_text.py
@@ -1,11 +1,22 @@
 from googletrans import Translator
 
+
 def translate_text(text, target_language):
     translator = Translator()
+
     try:
+        if not text:
+            raise ValueError("Invalid input: Text is empty.")
+        
         translated = translator.translate(text, dest=target_language)
-        print("-------- google trans -------")
-        print(translated.text)
+        if  translated is None or  translated.text is None:
+            raise ValueError("Translation result is empty or None.")
+        # print(f"Raw response: {translated}")
         return translated.text
     except Exception as e:
-        raise Exception(f"Error: {str(e)}")
+        # print(f"Error during translation: {str(e)}")
+        # print(f"Failed text: {text}")
+        # print(f"Target language: {target_language}")
+        return text
+
+
