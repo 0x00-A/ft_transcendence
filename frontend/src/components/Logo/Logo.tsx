39d9ac72ab902f7d22eaa7878bd86e6b347fc313
rel-isma <relismaiyly@gmail.com>
2025-01-04 21:59:13 +0100

    add logo

--- frontend/src/components/Logo/Logo.tsx
+++ frontend/src/components/Logo/Logo.tsx
@@ -1,14 +1,31 @@
-// import { NavLink } from 'react-router-dom';
-// import css from './Logo.module.css';
-// import LogoSVG from '/icons/logo.svg';
+import { useEffect, useState } from "react";
 
-function Logo({ style }: { style: string }) {
-  // return (
-  //   <NavLink to="/" className={css.logo}>
-  //     <img className={style} src={LogoSVG} alt="logo" />
-  //   </NavLink>
-  // );
-  return <img className={style} src="/new-logo.png" alt="logo" />;
+interface LogoProps {
+  style: string;
+}
+
+function Logo({ style }: LogoProps) {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 1264);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 1264);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <img
+      className={style}
+      src={isLargeScreen ? "/logo/logo.png" : "/logo/logoLarge.png"}
+      alt="logo"
+    />
+  );
 }
 
 export default Logo;
