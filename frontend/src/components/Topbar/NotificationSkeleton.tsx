53fe0507486ab0770125c36bb22123433cabb0a9
rel-isma <relismaiyly@gmail.com>
2025-01-04 18:47:42 +0100

    add button load more notifcation in notification and add NotificationSkeleton add logo

--- frontend/src/components/Topbar/NotificationSkeleton.tsx
+++ frontend/src/components/Topbar/NotificationSkeleton.tsx
@@ -1,14 +1,11 @@
 const NotificationSkeleton = () => (
     <div className="animate-pulse">
-      {[1, 2, 3].map((index) => (
+      {[1, 2, 3, 4].map((index) => (
         <div key={index} className="px-4 py-3 border-b border-gray-600 last:border-b-0">
           <div className="flex justify-between items-start gap-4">
-            {/* Title skeleton */}
             <div className="h-6 bg-gray-600 rounded w-3/4"></div>
-            {/* Date skeleton */}
             <div className="h-3 bg-gray-600 rounded w-16"></div>
           </div>
-          {/* Message skeleton */}
           <div className="mt-2 space-y-2">
             <div className="h-4 bg-gray-600 rounded w-full"></div>
             <div className="h-4 bg-gray-600 rounded w-2/3"></div>
