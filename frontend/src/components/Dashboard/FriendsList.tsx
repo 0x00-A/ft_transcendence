b014bc29de0ab8dcaa0d8ae3b9e8fbd85ab34be4
rel-isma <relismaiyly@gmail.com>
2025-01-05 14:16:51 +0100

    merge

--- frontend/src/components/Dashboard/FriendsList.tsx
+++ frontend/src/components/Dashboard/FriendsList.tsx
@@ -3,9 +3,13 @@ import { useGetData } from '@/api/apiHooks';
 import { Friends } from '@/types/apiTypes';
 import { API_GET_DASHBOARD_FRIENDS_URL } from '@/api/apiConfig';
 import { useTranslation } from 'react-i18next';
-import { UserPlus } from 'lucide-react';
+import { Swords, UserPlus } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import FriendSkeleton from '../Friends/FriendSkeleton';
+import { useEffect, useState } from 'react';
+import { useWebSocket } from '@/contexts/WebSocketContext';
+import { useUser } from '@/contexts/UserContext';
+
 
 const FriendsList = ({ username }: { username: string | undefined }) => {
   const {
@@ -15,6 +19,39 @@ const FriendsList = ({ username }: { username: string | undefined }) => {
   } = useGetData<Friends[]>(`${API_GET_DASHBOARD_FRIENDS_URL}${username}`);
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const [isInviteDisabled, setIsInviteDisabled] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(0);
+  const { sendMessage } = useWebSocket();
+  const { user } = useUser();
+
+
+  const handleSendInvite = (username: string) => {
+    if (isInviteDisabled) return;
+    sendMessage({
+      event: 'game_invite',
+      from: user?.username,
+      to: username,
+    });
+
+    setIsInviteDisabled(true);
+    setTimeLeft(10);
+  };
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (isInviteDisabled && timeLeft > 0) {
+      timer = setInterval(() => {
+        setTimeLeft((prev) => prev - 1);
+      }, 1000);
+    }
+
+    if (timeLeft === 0 && isInviteDisabled) {
+      setIsInviteDisabled(false);
+    }
+
+    return () => clearInterval(timer);
+  }, [isInviteDisabled, timeLeft]);
 
   return (
     <>
@@ -66,6 +103,18 @@ const FriendsList = ({ username }: { username: string | undefined }) => {
                 >
                   <span className={css.statusIndicator}></span>
                   {friend.profile.is_online ? t('dashboard.FriendsList.friendItem.status.online') : t('dashboard.FriendsList.friendItem.status.offline')}
+                </div>
+
+                <div
+                  className={`${css.challenge} ${isInviteDisabled ? css.disabled : ''}`}
+                  onClick={() => handleSendInvite(friend.username)}
+                >
+                    <Swords color="#f8c35c"/>
+                    {isInviteDisabled ? (
+                      <span className={css.cooldownTimer}>{timeLeft}s</span>
+                    ) : (
+                      <span>{t('Profile.profileHeader.buttons.challengeBtn')}</span>
+                    )}
                 </div>
               </div>
             ))}
