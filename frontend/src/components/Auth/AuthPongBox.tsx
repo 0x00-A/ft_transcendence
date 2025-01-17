b6372bc0a7ea527808e5324425bc844096ca90e1
0x00-A <abdellatifigounad@gmail.com>
2025-01-17 05:18:13 +0100

    added a link to github rep at signup page

--- frontend/src/components/Auth/AuthPongBox.tsx
+++ frontend/src/components/Auth/AuthPongBox.tsx
@@ -1,10 +1,12 @@
 // Styles
-import css from './AuthPongBox.module.css';
-import { LOGO } from '@/config/constants';
+import css from "./AuthPongBox.module.css";
+import { LOGO } from "@/config/constants";
+import { Github } from "lucide-react";
 
-const AuthPongBox = ({isLogin}: {isLogin: boolean}) => {
+const AuthPongBox = ({ isLogin }: { isLogin: boolean }) => {
   return (
-    <div className={`${css.authPongBox} ${!isLogin ? css.switchPongBox : ''}`}>
+    <div className={`${css.authPongBox} ${!isLogin ? css.switchPongBox : ""}`}>
+      {/* <div className="flex flex-col items-center"> */}
       <div className={css.logoHolder}>
         <img src={LOGO} alt="" />
       </div>
@@ -13,14 +15,23 @@ const AuthPongBox = ({isLogin}: {isLogin: boolean}) => {
         <div></div>
         <div></div>
       </div>
-      {/* <div className={css.authFooter}>
-        <p>{isLogin ? "Don't have an account?" : 'Already have an account?'}</p>
-        <button onClick={() => { setIslogin(!isLogin);}}>
-          {isLogin ? 'Signup' : 'Login'}
-        </button>
-      </div> */}
+      {/* </div> */}
+      <footer className="w-full py-6 px-4 mb-10">
+        <div className="w-full mx-auto flex items-center justify-center space-x-2 text-[#f8f3e3]">
+          {/* <span>Want to contribute?</span> */}
+          <a
+            href="https://github.com/0x00-A/ft_transcendence"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-flex items-center space-x-2 text-[#445c85] hover:text-blue-800 transition-colors"
+          >
+            <Github size={20} />
+            <span>Check out the code on GitHub</span>
+          </a>
+        </div>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default AuthPongBox
+export default AuthPongBox;
